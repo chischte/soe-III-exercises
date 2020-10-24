fcfaa3e660e5a41bec93415dad1690844b5daf58
michi <chischte@gmx.ch>
2020-10-24 14:16:18 +0200

    BKP

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -19,9 +19,11 @@ namespace MvcMovie.Controllers
         }
 
         // GET: /HelloWorld/Welcome/ 
-        public string Welcome(string name, int numTimes = 1)
+        // https://localhost:44324/HelloWorld/Welcome?name=Rick&numtimes=4
+        // https://localhost:44324/HelloWorld/Welcome/3?name=Rick
+        public string Welcome(string name, int ID = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
         
 
